5e6288d591a944206f6598acf3bc261ea00b8c47
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2023-12-29 10:44:34 -0300

    feat: Create status codes file

---
+++ backend/src/utils/StatusCodes.ts
@@ -0,0 +1,11 @@
+enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    NOT_FOUND = 404,
+    UNPROCESSABLE = 422,
+  }
+  
+  export default StatusCode;
