be7407dca1430b66778557299d123c00e3aa4901
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2023-12-29 10:44:09 -0300

    feat: Add routes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as cors from 'cors';
-// import routes from './routes';
+import routes from '../src/routes';
 
 class App {
   public app: express.Express;
@@ -14,17 +14,10 @@ class App {
   }
 
   private config():void {
-    const accessControl: express.RequestHandler = (_req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-      res.header('Access-Control-Allow-Headers', '*');
-      next();
-    };
     this.app.use(cors());
     this.app.use(express.json());
-    this.app.use(accessControl);
 
-    // this.app.use(routes);
+    this.app.use(routes);
   }
 
   public start(PORT: string | number):void {
