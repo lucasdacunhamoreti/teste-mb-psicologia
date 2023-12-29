d7868b2ceb21f166d879b882facdc3879eaaeb2a
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2023-12-28 23:56:26 -0300

    feat: Create database connection

---
+++ backend/src/database/config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'subjects',
+  host: process.env.MYSQL_HOST || 'localhost',
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+};
